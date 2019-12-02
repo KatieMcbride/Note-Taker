41c101b193e109d59146e078f75d26331a8b8f97
Katie McBride <mcbride.katieb@gmail.com>
2019-12-01 20:46:06 -0800

    server.js created

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,100 +1,19 @@
-// Dependencies
-// ===========================================================
-// var express = require("express");
-// var app = express();
-// const path = require("path");
-
-var noteTitle = (".note-title");
-var noteText = (".note-textarea");
-var saveNoteBtn = (".save-note");
-var newNoteBtn = (".new-note");
-var noteList = (".list-container .list-group");
-
-
+var $noteTitle = $(".note-title");
+var $noteText = $(".note-textarea");
+var $saveNoteBtn = $(".save-note");
+var $newNoteBtn = $(".new-note");
+var $noteList = $(".list-container .list-group");
 
 // activeNote is used to keep track of the note in the textarea
 var activeNote = {};
 
-
-var express = require("express");
-var app = express();
-const path = require("path");
-var PORT = process.env.PORT || 3000;
-var fs = require('fs');
-var index = require("./index");
-lastID = 1;
-
-//set up the express app to handle data parsing
-app.use(express.urlencoded({ extend : true }));
-app.use(express.json());
-app.use(express.static("public"));
-
-
-
-
-
-
-
-// createID(){
-//     return String.valueOf(idCounter++);
-// }; 
-
-// Routes
-// ===========================================================
-
-//General Route
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, '../../index.html'));
-});
-
-//Notes Route
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, '../../notes.html'));
-});
-
-var allNotes = fs.readFile('db.json', 'utf8',(err) => {
-    console.log('Data saved to db.json file');
-});
-console.log(allNotes);
-
-
-// Get all the data
-app.get('/api/notes', (req, res) =>{
-    return res.json(allNotes);
-});
-
- 
-// POST data
-app.post('/api/notes', (req, res) => {
-  objectID = lastID++;
-    console.log("Note received");
-    console.log(req.body);
-    const dataToPost = JSON.stringify(req.body);
-    fs.appendFileSync('../../../db/db.json',",\n" + objectID + dataToPost);
-    // fs.appendFile('db.json', dataToPost, (err) => {
-    //     console.log('Data saved to db.json file');
-    // });
-    res.send("Thank you!");
-}); 
-
-// DELETE data by ID
-app.post("/api/notes/:id", function(req, res) {
-  id = objectID;
-  id.delete();
-
-  res.json('Deleted note');
-});
-
-
-
-// A function for getting all notes from the db  
+// A function for getting all notes from the db
 var getNotes = function() {
   return $.ajax({
     url: "/api/notes",
     method: "GET"
   });
 };
-  
 
 // A function for saving a note to the db
 var saveNote = function(note) {
@@ -177,33 +96,33 @@ var handleNewNoteView = function() {
 // If a note's title or text are empty, hide the save button
 // Or else show it
 var handleRenderSaveBtn = function() {
-  if (!noteTitle.val().trim() || !noteText.val().trim()) {
-    saveNoteBtn.hide();
+  if (!$noteTitle.val().trim() || !$noteText.val().trim()) {
+    $saveNoteBtn.hide();
   } else {
-    saveNoteBtn.show();
+    $saveNoteBtn.show();
   }
 };
 
 // Render's the list of note titles
 var renderNoteList = function(notes) {
-  noteList.empty();
+  $noteList.empty();
 
   var noteListItems = [];
 
   for (var i = 0; i < notes.length; i++) {
     var note = notes[i];
 
-    var li = $("<li class='list-group-item'>").data(note);
-    var span = $("<span>").text(note.title);
-    var delBtn = $(
+    var $li = $("<li class='list-group-item'>").data(note);
+    var $span = $("<span>").text(note.title);
+    var $delBtn = $(
       "<i class='fas fa-trash-alt float-right text-danger delete-note'>"
     );
 
-    li.append(span, delBtn);
-    noteListItems.push(li);
+    $li.append($span, $delBtn);
+    noteListItems.push($li);
   }
 
-  noteList.append(noteListItems);
+  $noteList.append(noteListItems);
 };
 
 // Gets notes from the db and renders them to the sidebar
@@ -213,23 +132,12 @@ var getAndRenderNotes = function() {
   });
 };
 
-saveNoteBtn.on("click", handleNoteSave);
-noteList.on("click", ".list-group-item", handleNoteView);
-newNoteBtn.on("click", handleNewNoteView);
-noteList.on("click", ".delete-note", handleNoteDelete);
-noteTitle.on("keyup", handleRenderSaveBtn);
-noteText.on("keyup", handleRenderSaveBtn);
+$saveNoteBtn.on("click", handleNoteSave);
+$noteList.on("click", ".list-group-item", handleNoteView);
+$newNoteBtn.on("click", handleNewNoteView);
+$noteList.on("click", ".delete-note", handleNoteDelete);
+$noteTitle.on("keyup", handleRenderSaveBtn);
+$noteText.on("keyup", handleRenderSaveBtn);
 
 // Gets and renders the initial list of notes
 getAndRenderNotes();
-
-
-
-
-
-// Listener
-// ===========================================================
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
-});
-
--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,41 +1,62 @@
-// var express = require("express");
-// var app = express();
-// const path = require("path");
-// var PORT = process.env.PORT || 3000;
-// var fs = require('fs');
-// var index = require("./index");
+var express = require("express");
+var app = express();
+const path = require("path");
+var PORT = process.env.PORT || 3000;
+var fs = require('fs');
+var index = require("./index");
+lastID = 1;
 
-// //set up the express app to handle data parsing
-// app.use(express.urlencoded({ extend : true }));
-// app.use(express.json());
-// app.use(express.static("public"));
-
-
-// var allNotes = fs.readFile('db.json', 'utf8',(err) => {
-//     console.log('Data saved to db.json file');
-// });
-// console.log(allNotes);
+//set up the express app to handle data parsing
+app.use(express.urlencoded({ extend : true }));
+app.use(express.json());
+app.use(express.static("public"));
 
 
-// // Get all the data
-// app.get('/api/notes', (req, res) =>{
-//     return res.json(allNotes);
-// });
+//General Route
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, '../../index.html'));
+});
 
-// app.post('/api/notes', (req, res) => {
-//     console.log("Note received");
-//     console.log(req.body);
-//     const dataToPost = JSON.stringify(req.body);
-//     // fs.appendFileSync('db.json', dataToPost);
-//     fs.appendFile('db.json', dataToPost, (err) => {
-//         console.log('Data saved to db.json file');
-//     });
-//     res.send("Thank you!");
-// });   
+//Notes Route
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, '../../notes.html'));
+});
 
 
-// // Listener
-// // ===========================================================
-// app.listen(PORT, function() {
-//     console.log("App listening on PORT " + PORT);
-// });
+// Get all the data
+var allNotes = fs.readFile('db.json', 'utf8',(err) => {
+    console.log('Data saved to db.json file');
+});
+console.log(allNotes);
+
+app.get('/api/notes', (req, res) =>{
+    return res.json(allNotes);
+});  
+
+// POST data
+app.post('/api/notes', (req, res) => {
+    objectID = lastID++;
+      console.log("Note received");
+      console.log(req.body);
+      const dataToPost = JSON.stringify(req.body);
+      fs.appendFileSync('../../../db/db.json',",\n" + objectID + dataToPost);
+      // fs.appendFile('db.json', dataToPost, (err) => {
+      //     console.log('Data saved to db.json file');
+      // });
+      res.send("Thank you!");
+  }); 
+  
+// DELETE data by ID
+app.post("/api/notes/:id", function(req, res) {
+    id = objectID;
+    id.delete();
+  
+    res.json('Deleted note');
+  });
+
+
+// Listener
+// ===========================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
